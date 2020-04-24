5d1c59deeb6c34c85f5de316450237cfae40a7a5
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-04-24 00:03:18 -0300

    Testando JWT

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  useEffect(() => {
+    const signIn = async () => {
+      let token = localStorage.getItem('token')
+      if (!token) {
+        const login = await axios.post('http://localhost:3001/users/login', {
+          email: 'tuliofaria@devpleno.com',
+          passwd: 'abc123',
+        })
+        token = login.data.token
+        localStorage.setItem('token', token)
+      }
+      const decoded = jwtDecode(token)
+      console.log(decoded)
+    }
+    signIn()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
